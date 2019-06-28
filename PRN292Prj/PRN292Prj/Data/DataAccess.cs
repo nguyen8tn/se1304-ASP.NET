593967f4437500a277067be3b68727f060b3fa13
sonic2209 <51818304+sonic2209@users.noreply.github.com>
2019-06-28 15:53:47 +0700

    Create insertUser

--- PRN292Prj/PRN292Prj/Data/DataAccess.cs
+++ PRN292Prj/PRN292Prj/Data/DataAccess.cs
@@ -48,5 +48,34 @@ namespace PRN292Prj.Data
             }
             return role;
         }
+
+        public bool insertUser(User user)
+        {
+            bool check = false ;
+            string connectionString = Configuration.GetConnectionString("PRN292PrjContext");
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("sp_insertUser",conn);
+            cmd.Parameters.AddWithValue("@Username",user.Username);
+            cmd.Parameters.AddWithValue("@Password", user.Password);
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+            cmd.Parameters.AddWithValue("@Name", user.Name);
+            cmd.Parameters.AddWithValue("@Gender", user.Gender);
+            cmd.Parameters.AddWithValue("@Role", user.Role);
+            cmd.Parameters.AddWithValue("@DateOfCreate", user.DOC);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }catch(Exception e)
+            {
+                check = false;
+                throw new Exception(e.Message);
+            }
+            return check;
+        }
     }
 }
